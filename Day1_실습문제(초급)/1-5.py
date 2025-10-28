3bf3174820ca4db04f0034b07a120add6578878e
wnstj999 <wnstj99999@gmail.com>
2025-10-28 23:16:14 +0900

    123

--- Day1_실습문제(초급)/1-5.py
+++ Day1_실습문제(초급)/1-5.py
@@ -1,11 +1,21 @@
 a = 15
 b = 27
-c = 19
-if a > b and a > c:
-    print(f"{a}가 가장 큽니다.")
-elif b > a and b > c:
-    print(f"{b}가 가장 큽니다.")
-else:
-    print(f"{c}가 가장 큽니다.")
+c = 191
+# if a > b and a > c:
+#     print(f"{a}가 가장 큽니다.")
+# elif b > a and b > c:
+#     print(f"{b}가 가장 큽니다.")
+# else:
+#     print(f"{c}가 가장 큽니다.")
 
-print(f"{a if a > b and a > c else b if b > c and b > a else c}가 가장 큽니다.")
+# print(f"{a if a > b and a > c else b if b > c and b > a else c}가 가장 큽니다.")
+
+
+largest = a
+
+if b > largest:
+    largest = b
+if c > largest:
+    largest = c
+
+print(f"{largest}가 가장 큽니다.")
